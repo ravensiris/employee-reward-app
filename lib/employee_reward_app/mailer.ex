2542ed4471a4c08cc9a5ffe9bb0d33aba0c7eeb6
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-19 18:42:39 +0100

    :wrench: Setup mailer

--- lib/employee_reward_app/mailer.ex
+++ lib/employee_reward_app/mailer.ex
@@ -1,3 +1,22 @@
 defmodule EmployeeRewardApp.Mailer do
   use Swoosh.Mailer, otp_app: :employee_reward_app
+  require Logger
+  import Swoosh.Email
+
+  def cast(%{user: user, subject: subject, text: text, html: html, assigns: _assigns}) do
+    # Build email struct to be used in `process/1`
+    new()
+    |> from("noreply@example.org")
+    |> to(user.email)
+    |> subject(subject)
+    |> text_body(text)
+    |> html_body(html)
+  end
+
+  def process(email) do
+    # Send email
+
+    deliver!(email)
+    Logger.debug("E-mail sent: #{inspect(email)}")
+  end
 end
