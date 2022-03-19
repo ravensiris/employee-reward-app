e62337230d9302e372b3a2ade6ce1e7a2a9ab8b6
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-19 22:55:28 +0100

    :wrench: Install pow_assent
    mix pow_assent.install --binary-id

---
+++ lib/employee_reward_app/user_identities/user_identity.ex
@@ -0,0 +1,12 @@
+defmodule EmployeeRewardApp.UserIdentities.UserIdentity do
+  use Ecto.Schema
+  use PowAssent.Ecto.UserIdentities.Schema, user: EmployeeRewardApp.Users.User
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "user_identities" do
+    pow_assent_user_identity_fields()
+
+    timestamps()
+  end
+end
