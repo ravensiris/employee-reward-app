43a47468e1b29691e32154abb43ac6f907a1d9f5
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-19 17:56:29 +0100

    :card_file_box: Use binary_id on User

--- lib/employee_reward_app/users/user.ex
+++ lib/employee_reward_app/users/user.ex
@@ -2,6 +2,8 @@ defmodule EmployeeRewardApp.Users.User do
   use Ecto.Schema
   use Pow.Ecto.Schema
 
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
   schema "users" do
     pow_user_fields()
 
--- priv/repo/migrations/20220319164731_create_users.exs
+++ priv/repo/migrations/20220319164731_create_users.exs
@@ -2,7 +2,8 @@ defmodule EmployeeRewardApp.Repo.Migrations.CreateUsers do
   use Ecto.Migration
 
   def change do
-    create table(:users) do
+    create table(:users, primary_key: false) do
+      add :id, :binary_id, primary_key: true
       add :email, :string, null: false
       add :password_hash, :string, redact: true
 
