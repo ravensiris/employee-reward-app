79e91f0df909c5ac79d3ca1cb44e0fd8e0e2aee3
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-20 18:08:55 +0100

    :lipstick: Add BalanceSummary to Home

--- assets/src/pages/Home.tsx
+++ assets/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import BalanceSummary from "$/components/BalanceSummary";
 import Hello from "$components/Hello";
 import Navbar from "$components/Navbar";
 
@@ -6,6 +7,7 @@ export default function Home() {
     <>
       <Navbar></Navbar>
       <Hello />
+      <BalanceSummary />
     </>
   );
 }
