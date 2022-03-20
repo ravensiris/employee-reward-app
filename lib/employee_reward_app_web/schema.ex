8aa6acb74bafc4488deb1a7255dc5ca6fba4f236
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-20 01:16:27 +0100

    :construction: Fetching user from absinthe

--- lib/employee_reward_app_web/schema.ex
+++ lib/employee_reward_app_web/schema.ex
@@ -1,9 +1,25 @@
 defmodule EmployeeRewardAppWeb.Schema do
   use Absinthe.Schema
+  alias EmployeeRewardAppWeb.Schema
+
+  import_types(Schema.Types.UUID4)
+
+  object :user do
+    field :id, :uuid4
+    field :email, :string
+    field :name, :string
+  end
 
   query do
-    field :test, :string do
-      resolve(fn _, _, _ -> {:ok, "ok"} end)
+    field :test, :user do
+      resolve(fn _, _, %{context: context} ->
+        user = Map.get(context, :current_user)
+
+        case user do
+          nil -> {:error, "user not logged in"}
+          user -> {:ok, Map.take(user, [:id, :email, :name])}
+        end
+      end)
     end
   end
 end
