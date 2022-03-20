ccda8021a6626f1a3a2ad61bc6d5fb4596d5346b
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-20 17:07:37 +0100

    :lipstick: Create Home page

---
+++ assets/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import Hello from "$components/Hello";
+import Navbar from "$components/Navbar";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Hello />
+    </>
+  );
+}
