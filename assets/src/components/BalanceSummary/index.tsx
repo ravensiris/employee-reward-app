7226eda8637c7e48b688ac145e7a2d89afb3a1f6
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-20 18:06:59 +0100

    :lipstick: Create BalanceSummary component

---
+++ assets/src/components/BalanceSummary/index.tsx
@@ -0,0 +1,13 @@
+import Balance from "./Balance";
+import Received from "./Received";
+import Sent from "./Sent";
+
+export default function BalanceSummary() {
+  return (
+    <section className="section tile is-ancestor">
+      <Balance />
+      <Received />
+      <Sent />
+    </section>
+  );
+}
