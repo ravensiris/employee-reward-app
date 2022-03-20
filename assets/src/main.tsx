254fa37590759784ca1ff0231d216858a40d271e
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-20 12:36:53 +0100

    :construction: Test Apollo

--- assets/src/main.tsx
+++ assets/src/main.tsx
@@ -3,10 +3,40 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import './sass/app.sass';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
+
+const ME_QUERY = gql`
+  query getMe {
+    me {
+      id
+      email
+      role
+    }
+  }
+`;
+
+function Me(){
+  const {loading, error, data} = useQuery(ME_QUERY);
+  if (loading) return <p>Loading</p>;
+  if (error) return <p>Error</p>;
+
+  return <h1>Welcome {data.me.email}!</h1>;
+}
+
+const client = new ApolloClient({
+  uri: '/api',
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <Me />
+  </ApolloProvider>,
   document.getElementById('root')
 )
