60ddb6537bd8cb7b2e9ea7617b4dfe8970ccc06a
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-20 17:05:39 +0100

    :recycle: Cleanup App.tsx

--- assets/src/App.tsx
+++ assets/src/App.tsx
@@ -1,26 +1,13 @@
-import { useState, useCallback } from "react";
-import logo from "./logo.svg";
+import Home from "$pages/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const increment = useCallback(() => {
-    setCount((count) => count + 1);
-  }, [setCount]);
-
   return (
-    <section className="section">
-      <div className="container">
-        <img src={logo} alt="React logo" width={120} />
-        <h1 className="title">Hello World</h1>
-        <p className="subtitle">
-          A React app running on top of <strong>Phoenix</strong> and with
-          support for <strong>Bulma</strong> and <strong>SASS</strong>!
-        </p>
-        <button className="button is-primary" onClick={increment}>
-          Click me: {count}
-        </button>
-      </div>
-    </section>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </>
   );
 }
 
