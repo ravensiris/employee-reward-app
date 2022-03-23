d2c307b679fc506b51ddad4173051facc231335e
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-23 04:45:38 +0100

    :bulb: Add spec for 'me' resolver

--- lib/employee_reward_app/resolvers/me.ex
+++ lib/employee_reward_app/resolvers/me.ex
@@ -2,6 +2,8 @@ defmodule EmployeeRewardApp.Resolvers.Me do
   @moduledoc """
   This module defines resolvers relating to the current user
   """
+  @spec show_me(any, any, %{:context => map}) ::
+          {:error, String.t()} | {:ok, map}
   def show_me(_parent, _args, %{context: context}) do
     user = Map.get(context, :current_user)
 
