c421caa9de91453f444138806e04f8dc23cca9ef
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-23 04:00:03 +0100

    :bulb: Fix spec

--- lib/employee_reward_app_web/views/layout_view.ex
+++ lib/employee_reward_app_web/views/layout_view.ex
@@ -5,6 +5,6 @@ defmodule EmployeeRewardAppWeb.LayoutView do
   # so we instruct Elixir to not warn if the dashboard route is missing.
   @compile {:no_warn_undefined, {Routes, :live_dashboard_path, 2}}
   @env Mix.env()
-  @spec dev_env? :: false
+  @spec dev_env? :: boolean
   def dev_env?, do: @env == :dev
 end
