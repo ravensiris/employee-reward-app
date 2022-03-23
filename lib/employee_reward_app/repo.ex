5caff782e4e916868668d0d17f68becd388eee0e
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-23 03:44:37 +0100

    :bulb: Add dialyzer attributes
    As seen on: https://github.com/jeremyjh/dialyxir/wiki/Phoenix-Dialyxir-Quickstart

--- lib/employee_reward_app/repo.ex
+++ lib/employee_reward_app/repo.ex
@@ -2,4 +2,6 @@ defmodule EmployeeRewardApp.Repo do
   use Ecto.Repo,
     otp_app: :employee_reward_app,
     adapter: Ecto.Adapters.Postgres
+
+  @dialyzer {:nowarn_function, rollback: 1}
 end
--- lib/employee_reward_app_web/views/page_view.ex
+++ lib/employee_reward_app_web/views/page_view.ex
@@ -1,3 +1,4 @@
 defmodule EmployeeRewardAppWeb.PageView do
   use EmployeeRewardAppWeb, :view
+  @dialyzer :no_match
 end
