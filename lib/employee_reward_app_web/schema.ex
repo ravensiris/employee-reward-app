4c6a30c593b9ec0f1b8e5de7111245b53b2bef5e
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-24 22:53:02 +0100

    Use Authentication middleware in 'me'

--- lib/employee_reward_app_web/schema.ex
+++ lib/employee_reward_app_web/schema.ex
@@ -2,12 +2,14 @@ defmodule EmployeeRewardAppWeb.Schema do
   @moduledoc false
   use Absinthe.Schema
   alias EmployeeRewardAppWeb.Schema
+  alias EmployeeRewardAppWeb.Middleware
   alias EmployeeRewardApp.Resolvers
 
   import_types(Schema.Types.User)
 
   query do
     field :me, :user do
+      middleware(Middleware.Authentication)
       resolve(&Resolvers.Me.show_me/3)
     end
   end
