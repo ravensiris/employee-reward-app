792955bf3642390232d0602c7e3d27add70a05e7
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-24 02:02:04 +0100

    :lipstick: React takes control of Pow sign in page

--- assets/src/App.tsx
+++ assets/src/App.tsx
@@ -1,4 +1,5 @@
 import Home from "$pages/Home";
+import SignIn from "$pages/SignIn";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -6,6 +7,8 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />}></Route>
+        <Route path="/session" element={<SignIn />}></Route>
+        <Route path="/session/new" element={<SignIn />}></Route>
       </Routes>
     </>
   );
