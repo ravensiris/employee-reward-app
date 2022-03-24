d1147332270ee6c50c6d8811c6eb3db973ebb87a
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-24 21:23:35 +0100

    :card_file_box: Update schema & changesets

--- lib/employee_reward_app/users/user.ex
+++ lib/employee_reward_app/users/user.ex
@@ -12,20 +12,39 @@ defmodule EmployeeRewardApp.Users.User do
   @type t :: %__MODULE__{
           id: Ecto.UUID.t() | nil,
           email: String.t() | nil,
-          role: role() | nil
+          role: role() | nil,
+          name: String.t() | nil
         }
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "users" do
     pow_user_fields()
     field :role, Ecto.Enum, values: [:member, :admin], default: :member
+    field :name, :string
 
     timestamps()
+  end
+
+  @doc """
+  Overrides changeset for PowAssent
+
+  See: https://github.com/pow-auth/pow_assent#populate-fields
+  """
+  def user_identity_changeset(user_or_changeset, user_identity, attrs, user_id_attrs) do
+    user_or_changeset
+    |> Ecto.Changeset.cast(attrs, [:name])
+    |> pow_assent_user_identity_changeset(
+      user_identity,
+      attrs,
+      user_id_attrs
+    )
   end
 
   def changeset(user_or_changeset, attrs) do
     user_or_changeset
     |> pow_changeset(attrs)
     |> pow_extension_changeset(attrs)
+    |> Ecto.Changeset.cast(attrs, [:role, :name])
+    |> Ecto.Changeset.validate_required([:name])
   end
 end
