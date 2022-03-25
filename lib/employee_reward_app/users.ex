5d5101e383465e9b975220d311dbb05abae6db0b
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-25 01:07:12 +0100

    Implement user fuzzy search

---
+++ lib/employee_reward_app/users.ex
@@ -0,0 +1,19 @@
+defmodule EmployeeRewardApp.Users do
+  import Ecto.Query
+  alias EmployeeRewardApp.Users.User
+  alias EmployeeRewardApp.Repo
+
+  @spec search_users(String.t()) :: [User.t()]
+  def search_users(name) do
+    start_character = String.slice(name, 0..1)
+
+    from(
+      u in User,
+      where: ilike(u.name, ^"#{start_character}%"),
+      where: fragment("SIMILARITY(?, ?) > 0", u.name, ^name),
+      order_by: fragment("LEVENSHTEIN(?, ?)", u.name, ^name),
+      limit: 5
+    )
+    |> Repo.all()
+  end
+end
