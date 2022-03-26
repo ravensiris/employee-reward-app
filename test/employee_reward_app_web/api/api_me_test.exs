43682a074be4ad8ad42abfd34a31c5e7c891fa9d
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-26 22:39:49 +0100

    :white_check_mark: Move member_conn and admin_conn

--- test/employee_reward_app_web/api/api_me_test.exs
+++ test/employee_reward_app_web/api/api_me_test.exs
@@ -12,31 +12,6 @@ defmodule EmployeeRewardAppWeb.APIMeTest do
   }
   """
 
-  setup %{conn: conn} do
-    user = %EmployeeRewardApp.Users.User{
-      email: "john.doe@example.org",
-      id: "ea35faa3-a465-41a5-a6eb-6e9abef58326",
-      name: "John Doe"
-    }
-
-    member_conn = Pow.Plug.assign_current_user(conn, user, [])
-
-    {:ok, conn: conn, member_conn: member_conn}
-  end
-
-  setup %{conn: conn} do
-    admin = %EmployeeRewardApp.Users.User{
-      email: "admin@example.org",
-      id: "1e064793-ea8d-4110-b2e1-4d8e570ab7df",
-      role: :admin,
-      name: "Admin"
-    }
-
-    admin_conn = Pow.Plug.assign_current_user(conn, admin, [])
-
-    {:ok, conn: conn, admin_conn: admin_conn}
-  end
-
   defp query_me(conn) do
     post(conn, "/api", %{
       "query" => @me_query
--- test/support/conn_case.ex
+++ test/support/conn_case.ex
@@ -36,4 +36,29 @@ defmodule EmployeeRewardAppWeb.ConnCase do
     on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
     {:ok, conn: Phoenix.ConnTest.build_conn()}
   end
+
+  setup %{conn: conn} do
+    user = %EmployeeRewardApp.Users.User{
+      email: "john.doe@example.org",
+      id: "ea35faa3-a465-41a5-a6eb-6e9abef58326",
+      name: "John Doe"
+    }
+
+    member_conn = Pow.Plug.assign_current_user(conn, user, [])
+
+    {:ok, conn: conn, member_conn: member_conn}
+  end
+
+  setup %{conn: conn} do
+    admin = %EmployeeRewardApp.Users.User{
+      email: "admin@example.org",
+      id: "1e064793-ea8d-4110-b2e1-4d8e570ab7df",
+      role: :admin,
+      name: "Admin"
+    }
+
+    admin_conn = Pow.Plug.assign_current_user(conn, admin, [])
+
+    {:ok, conn: conn, admin_conn: admin_conn}
+  end
 end
