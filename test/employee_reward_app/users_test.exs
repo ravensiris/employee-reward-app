3d1f1a101a4188832c4a7c68005e8f844d6dcee5
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-26 22:19:41 +0100

    :white_check_mark: Rewrite search_users/1 test

--- test/employee_reward_app/users_test.exs
+++ test/employee_reward_app/users_test.exs
@@ -1,30 +1,59 @@
 defmodule EmployeeRewardApp.UsersTest do
   use EmployeeRewardApp.DataCase
+  import EmployeeRewardApp.Factory
   alias EmployeeRewardApp.Users
+  alias EmployeeRewardApp.Users.User
 
-  describe "user" do
-    alias EmployeeRewardApp.Users.User
-    alias EmployeeRewardApp.Repo
+  defp permutations([]), do: [[]]
 
-    @valid_attrs %{
-      name: "John Doe",
-      email: "john.doe@example.org",
-      password: "johnnyDoe123",
-      password_confirmation: "johnnyDoe123"
-    }
+  defp permutations(list),
+    do: for(elem <- list, rest <- permutations(list -- [elem]), do: [elem | rest])
 
-    def user_fixture(attrs \\ %{}) do
-      attrs = Map.merge(@valid_attrs, attrs)
+  describe "search_users/1" do
+    setup do
+      johns =
+        insert_list(10, :user, %{name: "John Doe"})
+        |> Enum.map(&Map.replace(&1, :password, nil))
 
-      %User{}
-      |> User.changeset(attrs)
-      |> Repo.insert()
+      {:ok, %{johns: johns}}
     end
 
-    test "search_user/1 finds user with fuzzy search" do
-      {:ok, user} = user_fixture()
+    setup %{johns: johns} do
+      john = List.last(johns)
+      {:ok, %{john: john}}
+    end
 
-      assert Users.search_users("jon") == [user]
+    setup %{john: john} do
+      john_clipped_id =
+        john.id
+        |> String.split("-")
+        |> List.first()
+
+      {:ok, %{john_clipped_id: john_clipped_id}}
+    end
+
+    test "find first 5 johns", %{johns: _johns} do
+      searched = Users.search_users("john")
+      assert length(searched) == 5
+    end
+
+    test "find specific john", %{john: john, john_clipped_id: john_clipped_id} do
+      assert Users.search_users("john@#{john_clipped_id}") == [john]
+    end
+
+    test "find specific john by id prefix", %{john: john, john_clipped_id: john_clipped_id} do
+      assert Users.search_users("@#{john_clipped_id}") == [john]
+    end
+
+    test "find johns with fuzzy matching" do
+      permuts =
+        String.graphemes("hn doe")
+        |> permutations()
+        |> Enum.map(fn n -> "jo" <> Enum.join(n) end)
+
+      template_result = Users.search_users("john doe")
+      results = Enum.map(permuts, &Users.search_users/1)
+      Enum.each(results, fn r -> assert r == template_result end)
     end
   end
 end
