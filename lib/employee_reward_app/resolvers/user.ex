09a2b7e472fb1675122c3f8a2453104062bdf811
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-26 12:01:08 +0100

    :bulb: Add more docs to search_user resolver

--- lib/employee_reward_app/resolvers/user.ex
+++ lib/employee_reward_app/resolvers/user.ex
@@ -4,6 +4,23 @@ defmodule EmployeeRewardApp.Resolvers.User do
   @moduledoc """
   This module defines resolvers relating to all users
   """
+
+  @spec search_user(any, %{:name => String.t(), optional(any) => any}, %{
+          :context => %{
+            :current_user => Users.User.t(),
+            :is_admin => boolean(),
+            optional(any) => any
+          },
+          optional(any) => any
+        }) :: {:ok, list(Users.User.t())}
+  @doc """
+  Resolver used for finding users by `name`.
+
+  Returns top 5 matching users except the current user.
+
+  `name` can include the id of the user as names are not unique and there could
+  be more than 5 John Smiths
+  """
   def search_user(_parent, %{name: name}, %{
         context: %{is_admin: is_admin, current_user: current_user}
       }) do
@@ -15,6 +32,7 @@ defmodule EmployeeRewardApp.Resolvers.User do
       else
         Enum.map(users, &Map.take(&1, [:id, :name]))
       end
+      # Remove current_user from results
       |> Enum.reject(fn user -> user.id == current_user.id end)
 
     {:ok, users}
