56ef5b008afb613c6b9bc14c361cd3e3adc362cd
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-26 21:29:15 +0100

    :bulb: Improve doc for search_users

--- lib/employee_reward_app/users.ex
+++ lib/employee_reward_app/users.ex
@@ -6,13 +6,64 @@ defmodule EmployeeRewardApp.Users do
 
   @spec search_users(String.t()) :: [User.t()]
   @doc """
-  Fuzzy search for a User.
+  Fuzzy search for a `User`.
 
-  `term` is User's name.
-  `term` can include User's id after '@' symbol
-  `term` can be just User's id if begins with '@' symbol
+  `term` is `User`'s `name` which is fuzzy matched
+  `term` can search using `User`'s `id` after '@' symbol
+  `term` can include both `User`'s `name` and `id` with '@' symbol between
 
-  Returns a list of matching Users
+  ## Examples
+  search_users("john")
+  [
+    #EmployeeRewardApp.Users.User<
+     ...
+     email: "john.doe@example.org",
+     id: "2db1d935-e5a5-4513-a247-5ccae2990ffc",
+     name: "John Doe",
+     role: :member,
+     ...
+    >,
+    #EmployeeRewardApp.Users.User<
+     ...
+     email: "john.smith@example.org",
+     id: "e9096aa8-7a53-481e-a310-08ffda9d21ab",
+     name: "John Smith",
+     role: :member,
+     ...
+    >,
+    #EmployeeRewardApp.Users.User<
+     ...
+     email: "john.joe.smith@example.org",
+     id: "47ae5412-a56d-43ec-91fd-3eeeddff637c",
+     name: "John Joe Smith",
+     role: :member,
+     ...
+    >
+  ]
+
+  search_users("john@e9")
+  [
+    #EmployeeRewardApp.Users.User<
+     ...
+     email: "john.smith@example.org",
+     id: "e9096aa8-7a53-481e-a310-08ffda9d21ab",
+     name: "John Smith",
+     role: :member,
+     ...
+    >
+  ]
+
+  search_users("@e9")
+  [
+    #EmployeeRewardApp.Users.User<
+     ...
+     email: "john.smith@example.org",
+     id: "e9096aa8-7a53-481e-a310-08ffda9d21ab",
+     name: "John Smith",
+     role: :member,
+     ...
+    >
+  ]
   """
   def search_users(term) do
     [name | rest] = String.split(term, "@")
