7bea70b59ea1d52ea52750727adbad75a890a702
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-26 20:14:02 +0100

    :heavy_plus_sign: Add faker

--- mix.exs
+++ mix.exs
@@ -60,7 +60,8 @@ defmodule EmployeeRewardApp.MixProject do
       {:absinthe_plug, "~> 1.5"},
       {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
-      {:ex_machina, "~> 2.7"}
+      {:ex_machina, "~> 2.7"},
+      {:faker, "~> 0.16", only: [:dev, :test]}
     ]
   end
 
