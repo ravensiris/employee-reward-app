0a5497f6b2cd85fd152fbc16f1fd92ed8b3010d5
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-26 12:00:53 +0100

    Improve user search query

--- lib/employee_reward_app/users.ex
+++ lib/employee_reward_app/users.ex
@@ -5,14 +5,35 @@ defmodule EmployeeRewardApp.Users do
   alias EmployeeRewardApp.Repo
 
   @spec search_users(String.t()) :: [User.t()]
-  def search_users(name) do
-    start_character = String.slice(name, 0..1)
+  @doc """
+  Fuzzy search for a User.
+
+  `term` is User's name, User's id or both
+
+  Returns a list of matching Users
+  """
+  def search_users(term) do
+    start_character = String.slice(term, 0..1)
 
     from(
       u in User,
+      # 2x where since term can start with either id or name
       where: ilike(u.name, ^"#{start_character}%"),
-      where: fragment("SIMILARITY(?, ?) > 0", u.name, ^name),
-      order_by: fragment("LEVENSHTEIN(?, ?) DESC", u.name, ^name),
+      or_where: ilike(type(u.id, :string), ^"#{start_character}%"),
+      # 2x where since term can start with either id or name
+      where:
+        fragment(
+          "SIMILARITY(?, ?) > 0",
+          u.name,
+          ^term
+        ),
+      or_where:
+        fragment(
+          "SIMILARITY(?, ?) > 0",
+          type(u.id, :string),
+          ^term
+        ),
+      order_by: fragment("LEVENSHTEIN(CONCAT(?, ?), ?) DESC", u.name, u.id, ^term),
       limit: 5
     )
     |> Repo.all()
