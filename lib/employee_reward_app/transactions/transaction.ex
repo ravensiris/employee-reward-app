046139a9c3778d103f22bda8198e9df38be84bd1
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-27 11:04:58 +0200

    :bulb: Add type to transaction schema

--- lib/employee_reward_app/transactions/transaction.ex
+++ lib/employee_reward_app/transactions/transaction.ex
@@ -2,6 +2,17 @@ defmodule EmployeeRewardApp.Transactions.Transaction do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @type status() :: :active | :cancelled
+
+  @type t :: %__MODULE__{
+          id: Ecto.UUID.t() | nil,
+          amount: integer() | nil,
+          status: status() | nil,
+          from_user: Ecto.UUID.t() | nil,
+          to_user: Ecto.UUID.t() | nil,
+          inserted_at: DateTime.t() | nil,
+          updated_at: DateTime.t() | nil
+        }
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   schema "transactions" do
