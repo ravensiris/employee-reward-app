5b2f37668e2b279c5e9f51ada65fba2fd18d9c64
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-27 12:20:17 +0200

    Create transaction factory

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -2,6 +2,7 @@ defmodule EmployeeRewardApp.Factory do
   @moduledoc false
   use ExMachina.Ecto, repo: EmployeeRewardApp.Repo
   alias EmployeeRewardApp.Users.User
+  alias EmployeeRewardApp.Transactions.Transaction
 
   def user_factory(attrs) do
     password = Map.get(attrs, :password, "arstarst")
@@ -14,5 +15,22 @@ defmodule EmployeeRewardApp.Factory do
     }
     |> merge_attributes(attrs)
     |> evaluate_lazy_attributes()
+  end
+
+  def transaction_factory do
+    now = Timex.now()
+    begin_month = Timex.beginning_of_month(now)
+    end_month = Timex.end_of_month(now)
+
+    inserted_at = Faker.DateTime.between(begin_month, end_month)
+
+    %Transaction{
+      amount: Enum.random(1..50),
+      from_user: build(:user),
+      to_user: build(:user),
+      status: sequence(:status, [:active, :cancelled]),
+      inserted_at: inserted_at,
+      updated_at: inserted_at
+    }
   end
 end
