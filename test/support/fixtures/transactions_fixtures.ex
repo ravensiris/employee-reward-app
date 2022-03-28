3f1628cc014034c58a68612d8c83885cbe52785b
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-28 15:05:14 +0200

    Revert "Create Transactions.Balance"
    This reverts commit ce5140c80c62622d7cee4604c61cd5174dff0b2d.
    
    Will work on balance after completing underlying subqueries/views

--- test/support/fixtures/transactions_fixtures.ex
+++ test/support/fixtures/transactions_fixtures.ex
@@ -18,21 +18,4 @@ defmodule EmployeeRewardApp.TransactionsFixtures do
 
     transaction
   end
-
-  @doc """
-  Generate a balance.
-  """
-  def balance_fixture(attrs \\ %{}) do
-    {:ok, balance} =
-      attrs
-      |> Enum.into(%{
-        balance: 42,
-        month: ~D[2022-03-26],
-        received: 42,
-        sent: 42
-      })
-      |> EmployeeRewardApp.Transactions.create_balance()
-
-    balance
-  end
 end
