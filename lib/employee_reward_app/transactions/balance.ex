bc1e46f06a50ba606c81d69a77071a50a68dd670
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-28 21:25:50 +0200

    Remove primary key and changeset on balance schema
    It's a read-only view, so no changeset.
    Changed the primary_key to user_id

--- lib/employee_reward_app/transactions/balance.ex
+++ lib/employee_reward_app/transactions/balance.ex
@@ -1,22 +1,11 @@
 defmodule EmployeeRewardApp.Transactions.Balance do
   use Ecto.Schema
-  import Ecto.Changeset
 
-  @primary_key {:id, :binary_id, autogenerate: true}
+  @primary_key {:user_id, :binary_id, []}
   @foreign_key_type :binary_id
   schema "transactions_balance" do
     field :balance, :integer
     field :received, :integer
     field :sent, :integer
-    field :user_id, :binary_id
-
-    timestamps()
-  end
-
-  @doc false
-  def changeset(balance, attrs) do
-    balance
-    |> cast(attrs, [:balance, :received, :sent])
-    |> validate_required([:balance, :received, :sent])
   end
 end
