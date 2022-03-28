5001331ac26527196f8a85e0896f78a0cd4e04ba
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-28 21:28:45 +0200

    Add type and moduledoc for Balance schema

--- lib/employee_reward_app/transactions/balance.ex
+++ lib/employee_reward_app/transactions/balance.ex
@@ -1,6 +1,15 @@
 defmodule EmployeeRewardApp.Transactions.Balance do
+  @moduledoc """
+  Represents `User`'s balance for the *current* month *ONLY*.
+  """
   use Ecto.Schema
 
+  @type t :: %__MODULE__{
+          user_id: Ecto.UUID.t() | nil,
+          balance: integer() | nil,
+          received: Ecto.UUID.t() | nil,
+          sent: Ecto.UUID.t() | nil
+        }
   @primary_key {:user_id, :binary_id, []}
   @foreign_key_type :binary_id
   schema "transactions_balance" do
