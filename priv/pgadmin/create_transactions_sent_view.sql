daf9e00bea11b5c4daf1c96371285ffdb8270afa
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-28 16:08:56 +0200

    :fire: Remove create_transactions_sent_view.sql

--- priv/pgadmin/create_transactions_sent_view.sql
+++
@@ -1,9 +0,0 @@
-CREATE VIEW transactions_sent
- AS
- SELECT transactions.from_user_id AS user_id,
-    sum(transactions.amount) AS sent,
-    EXTRACT(year FROM transactions.inserted_at) AS year,
-    EXTRACT(month FROM transactions.inserted_at) AS month,
-    transactions.status
-   FROM transactions
-  GROUP BY transactions.from_user_id, (EXTRACT(year FROM transactions.inserted_at)), (EXTRACT(month FROM transactions.inserted_at)), transactions.status;
