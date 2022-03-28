15e2604f0aee972d1c6dc375ba9b583de5169d8a
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-28 22:47:24 +0200

    Add forget_all to repo

--- lib/employee_reward_app/repo.ex
+++ lib/employee_reward_app/repo.ex
@@ -4,4 +4,50 @@ defmodule EmployeeRewardApp.Repo do
     adapter: Ecto.Adapters.Postgres
 
   @dialyzer {:nowarn_function, rollback: 1}
+  @doc """
+  Reverse a preload.
+  ## Example
+      site = Repo.get!(Site, 1) |> Repo.preload(:user)
+      Repo.forget(site, :user)
+  ## References
+  + https://stackoverflow.com/a/52323877
+  + https://stackoverflow.com/a/49997873
+  + https://github.com/thoughtbot/ex_machina/issues/295#issuecomment-433264227
+  """
+  def forget_all(struct) do
+    associations = resolve_associations(struct)
+    forget(struct, associations)
+  end
+
+  def forget(struct, associations) when is_list(associations) do
+    associations
+    |> Enum.reduce(struct, fn association, struct ->
+      forget(struct, association)
+    end)
+  end
+
+  def forget(struct, association) do
+    %{
+      struct
+      | association => build_not_loaded(struct, association)
+    }
+  end
+
+  defp resolve_associations(%{__struct__: schema}) do
+    schema.__schema__(:associations)
+  end
+
+  defp build_not_loaded(%{__struct__: schema}, association) do
+    %{
+      cardinality: cardinality,
+      field: field,
+      owner: owner
+    } = schema.__schema__(:association, association)
+
+    %Ecto.Association.NotLoaded{
+      __cardinality__: cardinality,
+      __field__: field,
+      __owner__: owner
+    }
+  end
 end
