ce4df9dcb32d065a95af0086e22a52bc6e245091
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-30 21:57:15 +0200

    Create get_recent_transactions inside Transactions

--- lib/employee_reward_app/transactions.ex
+++ lib/employee_reward_app/transactions.ex
@@ -38,8 +38,12 @@ defmodule EmployeeRewardApp.Transactions do
   """
   def get_transaction!(id), do: Repo.get!(Transaction, id)
 
+  # TODO: Add doc
+  # TODO: Add spec
   def get_balance!(user_id), do: Repo.get!(Balance, user_id)
 
+  # TODO: Add better doc
+  # TODO: Add spec
   @doc """
   Creates a transaction.
 
@@ -63,5 +67,36 @@ defmodule EmployeeRewardApp.Transactions do
     else
       default -> default
     end
+  end
+
+  defp with_direction(query \\ Transaction, user_id, direction) do
+    query
+    |> or_where([t], field(t, ^:"#{direction}_user_id") == ^user_id)
+  end
+
+  @type user_id() :: String.t() | Ecto.UUID.t()
+  @type direction() :: :from | :to
+
+  # TODO: Write tests
+  # TODO: Add doc
+  @spec get_recent_transactions(user_id()) :: [Transaction.t()]
+  def get_recent_transactions(user_id) do
+    get_recent_transactions(user_id, [:from, :to])
+  end
+
+  @spec get_recent_transactions(user_id(), direction()) :: [Transaction.t()]
+  def get_recent_transactions(user_id, direction) when is_atom(direction) do
+    get_recent_transactions(user_id, [direction])
+  end
+
+  @spec get_recent_transactions(user_id(), [direction()]) :: [Transaction.t()]
+  def get_recent_transactions(user_id, direction) when is_list(direction) do
+    query = Enum.reduce(direction, Transaction, fn dir, q -> with_direction(q, user_id, dir) end)
+
+    query
+    |> limit(10)
+    |> order_by([t], desc: t.inserted_at)
+    |> preload([:from_user, :to_user])
+    |> Repo.all()
   end
 end
