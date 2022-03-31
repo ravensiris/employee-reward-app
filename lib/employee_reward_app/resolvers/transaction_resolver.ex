26ade8d6be017c702be98395f3157a6c5b5309ab
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-31 02:49:08 +0200

    :passport_control: Handle errors & omit sensitive

--- lib/employee_reward_app/resolvers/transaction_resolver.ex
+++ lib/employee_reward_app/resolvers/transaction_resolver.ex
@@ -1,25 +1,35 @@
 defmodule EmployeeRewardApp.Resolvers.TransactionResolver do
   alias EmployeeRewardApp.Transactions
+  alias EmployeeRewardApp.Utils.Sensitive
 
   @moduledoc """
   This module defines resolvers relating to `Transactions`
   """
 
   # TODO: Spec
-  # BUG: 500 on any error, handle ecto fails
   def send_credits(_parent, %{to: to_user_id, amount: amount}, %{
         context: %{current_user: current_user}
       }) do
-    Transactions.create_transaction(%{
-      to_user_id: to_user_id,
-      amount: amount,
-      from_user_id: current_user.id
-    })
+    transaction =
+      Transactions.create_transaction(%{
+        to_user_id: to_user_id,
+        amount: amount,
+        from_user_id: current_user.id
+      })
+
+    with {:ok, t} <- transaction do
+      {:ok, t}
+    else
+      {:error, _} -> {:error, "unable to send credits"}
+    end
   end
 
   # TODO: Spec
-  # BUG: 500 on any error
   def get_recent(_parent, _args, %{context: %{current_user: current_user}}) do
-    {:ok, Transactions.get_recent_transactions(current_user.id)}
+    transactions =
+      Transactions.get_recent_transactions(current_user.id)
+      |> Sensitive.omit(current_user)
+
+    {:ok, transactions}
   end
 end
