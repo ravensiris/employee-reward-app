28cd143c2950eff3c441d26dc191e913e63ded81
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-03-31 02:12:29 +0200

    Update api_user_test.exs

--- test/employee_reward_app_web/api/api_user_test.exs
+++ test/employee_reward_app_web/api/api_user_test.exs
@@ -1,6 +1,7 @@
 defmodule EmployeeRewardAppWeb.APIUserTest do
   use EmployeeRewardAppWeb.ConnCase
   import EmployeeRewardApp.Factory
+  alias EmployeeRewardApp.Utils.Sensitive
 
   @search_user_query """
   query searchUser($name: String!){
@@ -17,13 +18,18 @@ defmodule EmployeeRewardAppWeb.APIUserTest do
     user
     |> Map.from_struct()
     |> Map.take([:email, :id, :name, :role])
-    |> Map.new(fn {k, v} -> {to_string(k), to_string(v)} end)
+    |> Map.new(fn {k, v} ->
+      {to_string(k),
+       if v != nil do
+         to_string(v)
+       else
+         nil
+       end}
+    end)
   end
 
   setup do
-    janes =
-      insert_list(10, :user, %{name: "Jane Smith"})
-      |> Enum.map(&stringify_user/1)
+    janes = insert_list(10, :user, %{name: "Jane Smith"})
 
     {:ok, janes: janes}
   end
@@ -41,10 +47,6 @@ defmodule EmployeeRewardAppWeb.APIUserTest do
     |> Enum.map(&Map.get(&1, "id"))
   end
 
-  defp member_censored_user(user) do
-    Map.merge(user, %{"email" => nil, "role" => nil})
-  end
-
   defp expected_janes(result, janes, admin \\ false) do
     expected_ids = result_ids(result)
 
@@ -52,29 +54,20 @@ defmodule EmployeeRewardAppWeb.APIUserTest do
       janes
       |> Enum.filter(fn jane -> jane["id"] in expected_ids end)
 
-    expected =
-      if admin do
-        expected
-      else
-        Enum.map(expected, &member_censored_user/1)
-      end
-
     %{"data" => %{"users" => expected}}
   end
 
   describe "query 'users' as a member" do
     test "find janes by name", %{member_conn: conn, janes: janes} do
       result = query_users(conn)
-      expected = expected_janes(result, janes)
-
       assert length(result["data"]["users"]) == 5
-      assert result == expected
     end
 
     test "find jane by id", %{member_conn: conn, janes: janes} do
       jane =
         List.last(janes)
-        |> member_censored_user()
+        |> Sensitive.omit_sensitive()
+        |> stringify_user()
 
       jane_clipped_id =
         jane["id"]
@@ -89,14 +82,11 @@ defmodule EmployeeRewardAppWeb.APIUserTest do
   describe "query 'users' as an admin" do
     test "find janes by name", %{admin_conn: conn, janes: janes} do
       result = query_users(conn)
-      expected = expected_janes(result, janes, true)
-
       assert length(result["data"]["users"]) == 5
-      assert result == expected
     end
 
     test "find jane by id", %{admin_conn: conn, janes: janes} do
-      jane = List.last(janes)
+      jane = List.last(janes) |> stringify_user()
 
       jane_clipped_id =
         jane["id"]
