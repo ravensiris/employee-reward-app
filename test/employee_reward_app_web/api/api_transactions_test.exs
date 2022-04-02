859f80a6382c7790dc3414fb5e117d6a47b960ee
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-04-02 16:15:49 +0200

    :white_check_mark: Add test for sending credits

--- test/employee_reward_app_web/api/api_transactions_test.exs
+++ test/employee_reward_app_web/api/api_transactions_test.exs
@@ -22,10 +22,34 @@ defmodule EmployeeRewardAppWeb.APITransactionsTest do
     }
   }
   """
+  @send_credits_mutation """
+  mutation sendCredits($to: UUID4!, $amount: Int!){
+    transaction(to: $to, amount: $amount){
+      amount,
+      toUser{
+        id
+        name
+        email
+      },
+      fromUser {
+        id
+        name
+        email
+      }
+    }
+  }
+  """
 
   defp query_recent(conn, query \\ @recent_query) do
     post(conn, "/api", %{
       "query" => query
+    })
+  end
+
+  defp mutation_send_credits(conn, to_user_id, amount, query \\ @send_credits_mutation) do
+    post(conn, "/api", %{
+      "query" => query,
+      "variables" => %{to: to_user_id, amount: amount}
     })
   end
 
@@ -58,6 +82,64 @@ defmodule EmployeeRewardAppWeb.APITransactionsTest do
       result_to = Enum.map(result, &Map.get(&1, "toUser"))
       result_to_emails = Enum.map(result_to, &Map.get(&1, "email"))
       assert Enum.all?(result_to_emails, &is_nil/1)
+    end
+  end
+
+  describe "mutation: send_credits" do
+    test "user can send credits", %{conn: conn} do
+      me = insert(:user, %{role: :member})
+      to_user = insert(:user)
+
+      result =
+        Pow.Plug.assign_current_user(conn, me, [])
+        |> mutation_send_credits(to_user.id, 1)
+        |> json_response(200)
+
+      assert result["data"]["transaction"] == %{
+               "amount" => 1,
+               "fromUser" => %{
+                 "email" => me.email,
+                 "id" => me.id,
+                 "name" => me.name
+               },
+               "toUser" => %{
+                 "email" => nil,
+                 "id" => to_user.id,
+                 "name" => to_user.name
+               }
+             }
+    end
+
+    test "user can't send more credits than he owns", %{conn: conn} do
+      # should have 50 credits by default
+      me = insert(:user, %{role: :member})
+      to_user = insert(:user)
+
+      result =
+        Pow.Plug.assign_current_user(conn, me, [])
+        |> mutation_send_credits(to_user.id, 51)
+        |> json_response(200)
+
+      assert result["data"]["transaction"] == nil
+      assert result["errors"]
+    end
+
+    test "user can't send invalid amount of credits", %{conn: conn} do
+      me = insert(:user, %{role: :member})
+      to_user = insert(:user)
+
+      conn = Pow.Plug.assign_current_user(conn, me, [])
+      edge_cases = [nil, -1, 0, "-1", "0", "1"]
+
+      for amount <- edge_cases do
+        result =
+          conn
+          |> mutation_send_credits(to_user.id, -1)
+          |> json_response(200)
+
+        assert result["data"]["transaction"] == nil
+        assert result["errors"]
+      end
     end
   end
 end
