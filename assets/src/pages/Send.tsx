cfa4efed9a3c75f0c605438b7082be3e76aeab2c
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-04-03 11:20:20 +0200

    Error handling and preventing invalid transactions

--- assets/src/pages/Send.tsx
+++ assets/src/pages/Send.tsx
@@ -1,25 +1,39 @@
 import CreditsInput from "$/components/CreditsInput"
+import PopupDialog from "$/components/PopupDialog"
 import RecentTransactions from "$/components/RecentTransactions"
 import { useSendCredits } from "$/operations/mutations/transactions"
 import AutocompleteUser from "$components/AutocompleteUser"
 import Navbar from "$components/Navbar"
 import { useRecentTransactions } from "$queries/transactions"
-import type { User } from "$queries/user"
+import { useMe, User } from "$queries/user"
 import React, { useState } from "react"
 
 export default function Send() {
   const [selectedUser, setSelectedUser] = useState<User>()
   const [amount, setAmount] = useState<number>(1)
-  const [sendCreditsMutation] = useSendCredits()
+  const [
+    sendCreditsMutation,
+    { error: sendingError, reset: resetSendingError },
+  ] = useSendCredits()
   const sendCredits = () => {
     sendCreditsMutation({ variables: { to: selectedUser?.id, amount: amount } })
   }
   const { data: transactionsData } = useRecentTransactions()
   const transactions = transactionsData?.transactions
+  const { data: meData } = useMe()
+  const balance = meData?.me.balance?.balance
 
   return (
     <>
       <Navbar />
+      <PopupDialog
+        onDismiss={resetSendingError}
+        isActive={!!sendingError}
+        title="Error"
+        type="is-danger"
+      >
+        {sendingError?.message}
+      </PopupDialog>
       <section className="section">
         <div className="container box">
           <div className="title">Send Ψ</div>
@@ -29,11 +43,19 @@ export default function Send() {
           <div className="block">
             <CreditsInput amount={amount} setAmount={setAmount} />
           </div>
+          {balance !== undefined && (
+            <div className="block">
+              <hr />
+              <p>Your balance: {balance}</p>
+            </div>
+          )}
           <div className="block">
             <button
               className="button is-primary w-full"
               onClick={sendCredits}
-              disabled={!selectedUser}
+              disabled={
+                !selectedUser || balance === undefined || balance < amount
+              }
             >
               Send
             </button>
@@ -42,7 +64,7 @@ export default function Send() {
       </section>
       <div className="section">
         <div className="box">
-          <div className="title">Sent Ψ</div>
+          <div className="title">Recently sent Ψ</div>
           {(transactions && (
             <RecentTransactions transactions={transactions} />
           )) || <div className="subtitle">No points sent yet!</div>}
