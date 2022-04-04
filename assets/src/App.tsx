b20d86069816e2665a31e176cdf5f4dbeed65065
Maksymilian Jodłowski <maksymilian.jodlowski@gmail.com>
2022-04-05 00:19:44 +0200

    :passport_control: Add authorization to all routes

--- assets/src/App.tsx
+++ assets/src/App.tsx
@@ -1,15 +1,40 @@
+import AuthorizedRoute from "$components/AuthorizedRoute"
 import Home from "$pages/Home"
 import Register from "$pages/Register"
 import Send from "$pages/Send"
 import SignIn from "$pages/SignIn"
+import { UserRole } from "$queries/user"
 import React from "react"
 import { Route, Routes } from "react-router-dom"
+import AdminOverview from "./pages/AdminOverview"
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/send" element={<Send />} />
+      <Route
+        path="/"
+        element={
+          <AuthorizedRoute>
+            <Home />
+          </AuthorizedRoute>
+        }
+      />
+      <Route
+        path="/send"
+        element={
+          <AuthorizedRoute>
+            <Send />
+          </AuthorizedRoute>
+        }
+      />
+      <Route
+        path="/admin/overview"
+        element={
+          <AuthorizedRoute role={UserRole.ADMIN}>
+            <AdminOverview />
+          </AuthorizedRoute>
+        }
+      />
       <Route path="/registration" element={<Register />} />
       <Route path="/registration/new" element={<Register />} />
       <Route path="/session" element={<SignIn />} />
